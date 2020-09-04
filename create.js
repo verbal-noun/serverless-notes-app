a8b726d2f78a2fa7948e968668cf196cd23361d9
verbal-noun <kaifahsan420@gmail.com>
2020-09-05 00:44:04 +1000

    Added AWS function & configured API endpoint

---
+++ create.js
@@ -0,0 +1,53 @@
+import * as uuid from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TableName,
+    // 'Item' contains the attributes of the item to be created
+    Item: {
+      // userId - user identities are federated through the Cognito identity pool
+      userId: event.requestContext.identity.cognitoIdentityId,
+      // noteId: a unique uuid
+      noteId: uuid.v1(),
+      // content: parsed from request body
+      content: data.content,
+      // attachment: parsed from request body
+      attachment: data.attachment,
+      //createdAt: current Unix timestamp
+      createdAt: Date.now(),
+    },
+  };
+
+  dynamoDb.put(params, (error, data) => {
+    const headers = {
+      // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    };
+
+    // Return status code 500 on error
+    if (error) {
+      const response = {
+        statusCode: 500,
+        headers: headers,
+        body: JSON.stringify({ status: false }),
+      };
+      callback(null, response);
+      return;
+    }
+
+    // Return status code 200 and the newly created item
+    const response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+}
